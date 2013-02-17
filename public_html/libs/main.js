838d1ee0d76e0859d6304ac27e47cc5bc11d9b4c
garwin <garwin@garwin-N55SL>
2013-02-16 22:59:58 -0500

    Learning HTML 5 - Storage API

--- public_html/libs/main.js
+++ public_html/libs/main.js
@@ -2,6 +2,28 @@ Ext.onReady(
 function()
 {
     Ext.Msg.alert("alert","Hello we are ready");
+    
+    if(typeof(Storage) !== "undefined"){
+       Ext.Msg.alert("Storage", "Yeah storage works!");
+       if(!localStorage.localCount)
+       {
+            localStorage.localCount = 0;
+            sessionStorage.sessionCount = 0;
+       }
+       
+       if(!sessionStorage.sessionCount)
+       {
+         sessionStorage.sessionCount =0;      
+       }
+       
+       localStorage.localCount = parseInt(localStorage.localCount) +1;
+       sessionStorage.sessionCount = parseInt(sessionStorage.sessionCount) +1;
+       
+       console.log("Local Storage Count:" + localStorage.localCount );
+       console.log("session Storage Count:" + sessionStorage.sessionCount );
+    }
+    else
+    {
     Ext.Msg.confirm("Question", "What is your answer?", 
     function(answer)
     {
@@ -13,7 +35,10 @@ function()
             {
                 Ext.Msg.alert("", "You clicked no!");
             }
+            
+            
     });
+    }
     
     var content = Ext.get("content");
     content.on("contextmenu",function(){Ext.Msg.alert("Just Testing!")});
